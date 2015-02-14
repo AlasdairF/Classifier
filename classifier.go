73f9d5cc5336d685f3a43589509c26929a4b79ca
AlasdairF <AlasdairF@users.noreply.github.com>
2015-02-14 23:07:40 +0700

    v3, major overhaul
    No longer uses map structure at all, uses binsearch package instead.
    Faster and much more efficient on memory.

--- classifier.go
+++ classifier.go
@@ -1,6 +1,7 @@
 package classifier
 
 import (
+ "github.com/AlasdairF/BinSearch"
  "github.com/AlasdairF/Custom"
  "os"
  "math"
@@ -32,27 +33,28 @@ const number_of_ensembles = 20
 
 type Trainer struct {
  Classifier // inherits Classifier struct
- testDocs [][][]string
+ testDocs [][]binsearch.CounterBytes
  numTestDocs int
- TrainingTokens [][]string
- Category_index map[string]int // Category_index can be useful as it contains a map of cat => index, where index is the slice index of the category in Classifier.Categories
+ trainingTokens [][]byte
+ categoryIndex binsearch.KeyValBytes
  ensembleContent [][]word
  ensembled bool
 }
 
 type Classifier struct {
- Categories []string
- rules map[string][]scorer
+ Categories [][]byte
+ rules binsearch.KeyBytes
+ res [][]scorer
 }
 
 type word struct {
- tok string
- score float32
+ tok []byte
+ score int
 }
 
 type scorer struct {
  category uint16
- score float32
+ score uint64
 }
 
 // --------------- FUNCTIONS ---------------
@@ -74,23 +76,25 @@ func randomList(num int, wanted int) []int {
 }
 
 // DefineCategories creates categories from a slice of categories names as strings.
-func (t *Trainer) DefineCategories(categories []string) {
-	// Reset & init
-	t.Category_index = make(map[string]int)
+func (t *Trainer) DefineCategories(categories [][]byte) error {
 	// Now generate forward and reverse index for categories <-> ensembles <-> indices
 	t.Categories = categories
 	for i, category := range categories {
-		t.Category_index[category] = i
+		if t.categoryIndex.AddUnsorted(category, i) != nil {
+			return errors.New(`Category name must be no more than 64 bytes.`)
+		}
 	}
-	t.testDocs = make([][][]string, len(categories))
-	t.TrainingTokens = make([][]string, len(categories))
+	t.categoryIndex.Build()
+	t.testDocs = make([][]binsearch.KeyValBytes, len(categories))
+	t.TrainingTokens = make([][]byte, len(categories))
+	return nil
 }
 
 // AddTrainingDoc adds a training document to the classifier.
-func (t *Trainer) AddTrainingDoc(category string, tokens []string) error {
+func (t *Trainer) AddTrainingDoc(category []byte, tokens [][]byte) error {
 	t.ensembled = false // Needs to be ensembled whenever a training doc is added
 	// Check to see if category exists already, if it doesn't then add it
-	indx, ok := t.Category_index[category]
+	indx, ok := t.categoryIndex.Find(category)
 	if !ok {
 		return errors.New(`AddTrainingDoc: Category '` + category + `' not defined`)
 	}
@@ -100,14 +104,20 @@ func (t *Trainer) AddTrainingDoc(category string, tokens []string) error {
 }
 
 // AddTestDoc adds a document for testing under the Test function.
-func (t *Trainer) AddTestDoc(category string, tokens []string) error {
+func (t *Trainer) AddTestDoc(category []byte, tokens [][]byte) error {
 	// Check to see if category exists already, if it doesn't then add it
-	indx, ok := t.Category_index[category]
+	indx, ok := t.categoryIndex.Find(category)
 	if !ok {
 		return errors.New(`AddTestDoc: Category '` + category + `' not defined`)
 	}
 	// Check capacity and grow if necessary
-	t.testDocs[indx] = append(t.testDocs[indx], tokens)
+	t.testDocs[indx] = append(t.testDocs[indx], binsearch.CounterBytes{})
+	obj := &t.testDocs[indx][len(t.testDocs[indx])-1]
+	for _, word := range tokens {
+		obj.Add(word, 1)
+	}
+	obj.Build()
+	
 	t.numTestDocs++
 	return nil
 }
@@ -116,54 +126,58 @@ func (t *Trainer) AddTestDoc(category string, tokens []string) error {
 func (t *Trainer) ensemble() {
 	// Initialize
 	nlist := make([]int, len(t.Categories) * number_of_ensembles)
-	tokmap := make([]map[string]uint, len(t.Categories) * number_of_ensembles)
-	bigmap := make(map[string]uint)
-	var i, i2, indx, ensembleindx, num_tokens, per_ensemble int
-	var total uint64
+	tokmap := make([]binsearch.CounterBytes, len(t.Categories) * number_of_ensembles)
+	ensembleTokAvg := new(binsearch.CounterBytes)
+	var i, i2, indx, ensembleindx, num_tokens, per_ensemble, total int
 	var tokloop []int
-	var tok string
+	var tok []byte
 	numcats := len(t.Categories)
 	// Loop through all categories of training docs
 	for indx=0; indx<numcats; indx++ {
 		// Generate 20x ensembles of 50% tokens
 		num_tokens = len(t.TrainingTokens[indx])
-		per_ensemble = (num_tokens+1)/2
+		per_ensemble = (num_tokens + 1) / 2
 		for i=0; i<number_of_ensembles; i++ {
 			tokloop = randomList(num_tokens, per_ensemble) // select 50% random sampling for this category
 			nlist[ensembleindx] = per_ensemble
-			total += uint64(per_ensemble)
-			tokmap[ensembleindx] = make(map[string]uint)
+			total += per_ensemble
+			//tokmap[ensembleindx] = make(map[string]uint)
 			for i2=0; i2<per_ensemble; i2++ {
 				tok = t.TrainingTokens[indx][tokloop[i2]]
-				tokmap[ensembleindx][tok]++
-				bigmap[tok]++
+				tokmap[ensembleindx].Add(tok, 1)
+				ensembleTokAvg.Add(tok, 1)
 			}
+			tokmap[ensembleindx].Build()
 			ensembleindx++
 		}
 	}
-	// And add to the overall counts
-	ensembleTokAvg := make(map[string]float32)
-	avg := float32(total)
-	for tok, count := range bigmap {
-		ensembleTokAvg[tok] = float32(count) / avg
-	}
-	var count uint
-	var l int
+	ensembleTokAvg.Build()
+	
+	// Calculate frequency for each token across all categories, multiplied by 10,000,000 so it will fit into an int instead of requiring a float
+	ensembleTokAvg.UpdateAll(func(v int) int {return (v * 10000000) / total})
+	
+	var count, l, av, v int
+	var eof, ok bool
 	// Now prune ensembleContent to remove all that are less than avg and therefore useless
 	t.ensembleContent = make([][]word, len(t.Categories) * number_of_ensembles)
 	var ensembleContent []word
 	ensembleindx = 0
 	for indx=0; indx<numcats; indx++ { // loop through categories
 		for i=0; i<number_of_ensembles; i++ { // loop through ensemble categories
-			l = len(tokmap[ensembleindx])
+			// Check the size of the temporary working array
+			l = tokmap[ensembleindx].Len()
 			if l > len(ensembleContent) {
 				ensembleContent = make([]word, l)
 			}
+			// Loop through all tokens in this ensemble
 			i2 = 0
-			for tok, count = range tokmap[ensembleindx] {
-				if count > 1 {
-					if avg = float32(count) / float32(nlist[ensembleindx]); avg > ensembleTokAvg[tok] {
-						ensembleContent[i2] = word{tok, avg / ensembleTokAvg[tok]}
+			for eof = false; !eof; {
+				tok, count, eof = tokmap[ensembleindx].Next() // get the next one
+				if count >= 2 { // there must be at least 2 occurances of this token in this ensemble
+					av = (count * 10000000) / nlist[ensembleindx] // Calculatate frequency for this token within this ensemble
+					v, ok = ensembleTokAvg.Find(tok) // what's the average for this token overall?
+					if av > v && ok { // if this token frequency in this ensemble is greater than average for all categories and ensembles
+						ensembleContent[i2] = word{tok, (av * 1000) / ensembleTokAvg[tok]} // the result is the percentage over the average that this tokens occurs in this ensemble, multiplied by 1000 so it fits. It will always be >1 (or in this case >1000)
 						i2++
 					}
 				}
@@ -185,56 +199,68 @@ func (t *Trainer) Create(allowance float32, maxscore float32) {
 		t.ensembled = true
 	}
 	// Now build the classifier
-	var i, i2, ensembleindx, l int
+	allowanceint, maxscoreint := int(allowance * 1000), int(maxscore * 1000)
+	var i, ensembleindx, score, l int
 	var indx16 uint16
-	var scorelog, score float32
+	var scorelog uint64
 	var old []scorer
-	var ok bool
-	var tok string
-	t.rules = make(map[string][]scorer)
+	var exists, eof bool
+	var tok []byte
+	rules := new(binsearch.KeyBytes)
+	res := make([]scorer, 0, 1000)
+	
 	for indx, _ := range t.Categories { // loop through categories
-		tally := make(map[string]float32) // create tally for scores from this category
+		tally := new(binsearch.CounterBytes) // create tally for scores from this category
 		for i=0; i<number_of_ensembles; i++ { // loop through ensemble categories
 			ensembleindx = (indx * number_of_ensembles) + i // get the index for this ensemble category
-			l = len(t.ensembleContent[ensembleindx]) // get the number of tokens in this ensemble category
-			for i2=0; i2<l; i2++ { // loop through all the tokens in this ensemble category
-				score = t.ensembleContent[ensembleindx][i2].score // calculate the score of this token
-				if score >= allowance { // If the score is greater than the allowance
-					if maxscore > 0 && score > maxscore { // if score is greater than the maximum allowed score for one token then reduce it to the maximum
-						score = maxscore
-					}
-					tally[t.ensembleContent[ensembleindx][i2].tok] += score // Add token and score to the tally for this category
+			for _, obj := range t.ensembleContent[ensembleindx] {
+				if obj.score >= allowanceint { // If the score is greater than the allowance
+					if maxscoreint > 0 && obj.score > maxscoreint { // if score is greater than the maximum allowed score for one token then reduce it to the maximum
+						tally.Add(obj.tok, maxscoreint)
+					} else {
+						tally.Add(obj.tok, obj.score)
 					}
 				}
 			}
+		}
+		tally.Build()
 		// Enter tallys into classifier
 		indx16 = uint16(indx)
-		for tok, score = range tally {
-			scorelog = float32(math.Log(float64(score)))
-			if old, ok = t.rules[tok]; ok {
-				i2 = len(old)
-				newone := make([]scorer, i2 + 1)
-				copy(newone, old)
-				newone[i2] = scorer{indx16, scorelog}
-				t.rules[tok] = newone
+		
+		for eof = false; !eof; {
+			tok, score, eof = tally.Next() // get the next one
+			scorelog = uint64(math.Log(float64(score) / 1000) * 1000)
+			i, exists = rules.Add(tok)
+			if exists {
+				res[i] = append(res[i], scorer{indx16, scorelog})
 			} else {
-				t.rules[tok] = []scorer{scorer{indx16, scorelog}}	
+				l = len(res)
+				if l == cap(res) {
+					tmp := make([][3]uint64, l + 1, (l * 2) + 1)
+					copy(tmp, res[0:i])
+					copy(tmp[i+1:], res[i:])
+					res = tmp
+				} else {
+					res = res[0:l+1]
+					copy(res[i+1:], res[i:])
+				}
+				res[i] = []scorer{scorer{index16, scorelog}}
 			}
 		}
 	}
 }
 
-// Classify classifies tokens and returns a slice of float32 where each index is the same as the index for the category name in classifier.Categories, which is the same as the []string of categories originally past to DefineCategories.
-func (t *Classifier) Classify(tokens []string) []float64 {
-	var tok string
+// Classify classifies tokens and returns a slice of uint64 where each index is the same as the index for the category name in classifier.Categories, which is the same as the []string of categories originally past to DefineCategories.
+func (t *Classifier) Classify(tokens [][]byte) []uint64 {
+	var tok []byte
 	var ok bool
-	var rules []scorer
+	var i int
 	var obj scorer
-	scoreboard := make([]float64, len(t.Categories))
+	scoreboard := make([]uint64, len(t.Categories))
 	for _, tok = range tokens {
-		if rules, ok = t.rules[tok]; ok {
-			for _, obj = range rules {
-				scoreboard[obj.category] += float64(obj.score)
+		if i, ok = t.rules.Find(tok); ok {
+			for _, obj = range t.res[i] {
+				scoreboard[obj.category] += obj.score
 			}
 		}
 	}
@@ -242,9 +268,9 @@ func (t *Classifier) Classify(tokens []string) []float64 {
 }
 
 // ClassifySimple is a wrapper for Classify, it returns the name of the best category as a string, and the score of the best category as float32.
-func (t *Classifier) ClassifySimple(tokens []string) (string, float64) {
+func (t *Classifier) ClassifySimple(tokens [][]byte) ([]byte, uint64) {
 	scoreboard := t.Classify(tokens)
-	var bestscore float64
+	var bestscore uint64
 	var bestcat int
 	for cat, score := range scoreboard {
 		if score > bestscore {
@@ -253,6 +279,31 @@ func (t *Classifier) ClassifySimple(tokens []string) (string, float64) {
 		}
 	}
 	return t.Categories[bestcat], bestscore
+}
+
+func (t *Trainer) classifyTestDoc(test *binsearch.CounterBytes) int {
+	var tok []byte
+	var v, i int
+	var eof, ok bool
+	var obj scorer
+	scoreboard := make([]uint64, len(t.Categories))
+	for !eof {
+		tok, v, eof = test.Next() // get the next one
+		if i, ok = t.rules.Find(tok); ok {
+			for _, obj = range t.res[i] {
+				scoreboard[obj.category] += obj.score * v
+			}
+		}
+	}
+	var bestscore uint64
+	i = 0
+	for cat, score := range scoreboard {
+		if score > bestscore {
+			bestscore = score
+			i = cat
+		}
+	}
+	return i
 }
 
 // Test tries 2,401 different combinations of allowance & maxscore then returns the values of allowance & maxscore which performs the best. Test requires an argument of true or false for verbose, if true Test will print all results to Stdout. 
@@ -264,8 +315,7 @@ func (t *Trainer) Test(verbose bool) (float32, float32, error) {
 	num_test_docs := float32(t.numTestDocs)
 	// Set some variables
 	var bestaccuracy, bestallowance, bestmaxscore, accuracy, allowance, maxscore float32
-	var i, indx, correct, l int
-	var cat, compare string
+	var i, indx, correct, l, compare int
 	// auto is the list of numbers to try for allowance and maxscore
 	var auto_allowance = [...]float32{0,1.05,1.1,1.15,1.2,1.25,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.5,3,4,5,6,7,8,9,10,15,20,25,30,40,50,75,100,150,200,300,400,500,600,700,800,900,1000,1500,2000,3000,4000,5000,10000,20000,50000,100000,1000000}
 	var auto_maxscore = [...]float32{0,10000000,1000000,100000,50000,20000,10000,5000,4000,3000,2000,1500,1200,1000,900,800,700,600,550,500,475,450,425,400,375,350,325,300,275,250,225,200,150,100,75,50,40,30,25,20,15,10,8,6,4,2}
@@ -274,10 +324,10 @@ func (t *Trainer) Test(verbose bool) (float32, float32, error) {
 			t.Create(allowance, maxscore) // build the classifier for allowance & maxscore
 			correct = 0
 			// Count the number of correct results from testDocs under this classifier
-			for indx, cat = range t.Categories {
+			for indx = range t.Categories {
 				l = len(t.testDocs[indx])
 				for i=0; i<l; i++ {
-					if compare, _ = t.ClassifySimple(t.testDocs[indx][i]); compare == cat {
+					if compare = t.classifyTestDoc(&t.testDocs[indx][i]); compare == indx {
 						correct++
 					}
 				}
@@ -328,53 +378,53 @@ func Load(filename string) (*Classifier, error) {
 	
 	var i uint16
 	numcats := r.Read16()
-	categories := make([]string, numcats)
+	categories := make([][]byte, numcats)
 	for i=0; i<numcats; i++ {
-		categories[i] = r.ReadString8()
+		categories[i] = r.Readx(int(r.Read8()))
 	}
 	numrules := r.Read64()
+	res := make([][]scorer, numrules)
 	
-	rules := make(map[string][]scorer)
-	var i2 uint64
-	var score float32
+	var i2, score uint64
 	var id, n uint16
-	var tok string
 	if numcats < 256 {
 		for i2=0; i2<numrules; i2++ {
-			tok = r.ReadString8()
 			n = uint16(r.Read8())
 			lst := make([]scorer, n)
 			for i=0; i<n; i++ {
 				id = uint16(r.Read8())
-				score = r.ReadFloat32()
+				score = r.Read64Variable()
 				lst[i] = scorer{id, score}
 			}
-			rules[tok] = lst
+			res[i2] = lst
 		}
 	} else {
 		for i2=0; i2<numrules; i2++ {
-			tok = r.ReadString8()
 			n = r.Read16()
 			lst := make([]scorer, n)
 			for i=0; i<n; i++ {
 				id = r.Read16()
-				score = r.ReadFloat32()
+				score = r.Read64Variable()
 				lst[i] = scorer{id, score}
 			}
-			rules[tok] = lst
+			res[i2] = lst
 		}
 	}
+	
+	// Create the new object
+	t := new(Classifier)
+	t.Categories = categories
+	t.res = res
+	
+	// Load binsearch.KeyBytes
+	t.rules.Read(r)
 	
 	// Make sure we're at the end and the checksum is OK
 	err = r.EOF()
 	if err != nil {
-		return nil, errors.New(`Not a valid string-based classifier file.`)
+		return nil, errors.New(`Not a valid classifier file.`)
 	}
 	
-	// Return the new object
-	t := new(Classifier)
-	t.Categories = categories
-	t.rules = rules
 	return t, nil
 }
 
@@ -389,35 +439,36 @@ func (t *Trainer) Save(filename string) error {
 	w := custom.NewWriter(fi)
 	defer w.Close()
 	
-	var tok string
 	var lst []scorer
-	var res scorer
+	var obj scorer
 	numcats := uint16(len(t.Categories))
 	
 	w.Write16(numcats)
 	for _, cat := range t.Categories {
-		w.WriteString8(cat)
+		w.WriteString8(string(cat))
 	}
-	w.Write64(uint64(len(t.rules)))
+	w.Write64(uint64(len(t.res)))
 
 	if numcats < 256 {
-		for tok, lst = range t.rules {
-			w.WriteString8(tok)
+		for _, lst = range t.res {
 			w.Write8(uint8(len(lst)))
-			for _, res = range lst {
-				w.Write8(uint8(res.category))
-				w.WriteFloat32(res.score)
+			for _, obj = range lst {
+				w.Write8(uint8(obj.category))
+				w.Write64Variable(obj.score)
 			}
 		}
 	} else {
-		for tok, lst = range t.rules {
-			w.WriteString8(tok)
+		for _, lst = range t.rules {
 			w.Write16(uint16(len(lst)))
-			for _, res = range lst {
-				w.Write16(res.category)
-				w.WriteFloat32(res.score)
+			for _, obj = range lst {
+				w.Write16(obj.category)
+				w.Write64Variable(obj.score)
 			}
 		}
 	}
+	
+	// Write the binsearch structure
+	t.rules.Write(w)
+	
 	return nil
 }
